89b0ec6b81b29f6ffb60a49b8c07af502ba92fef
Drew Stone <drewstone329@gmail.com>
2021-08-07 02:29:31 -0400

    Give admin to wrapper

--- contracts/tokens/TCRTokenWrapper.sol
+++ contracts/tokens/TCRTokenWrapper.sol
@@ -12,26 +12,32 @@ import "./TokenWrapper.sol";
     @author Webb Technologies.
     @notice This contract is intended to be used with ERC20Handler contract.
  */
-contract TCRTokenWrapper is TokenWrapper {  
+contract TCRTokenWrapper is TokenWrapper {
+  address governor;
   address[] tokens;
   mapping (address => bool) valid;
 
-  constructor(string memory name, string memory symbol)
-    TokenWrapper(name, symbol) {}
+  constructor(string memory name, string memory symbol) TokenWrapper(name, symbol) {
+    governor = msg.sender;
+  }
+
+  function setGovernor(address _governor) public onlyGovernor {
+    governor = _governor;
+  }
 
   /** @dev this function is defined in a child contract */
   function _isValid(address tokenAddress) override internal virtual returns (bool) {
     return valid[tokenAddress];
   }
 
-  function add(address tokenAddress) public onlyThis {
+  function add(address tokenAddress) public onlyGovernor {
     require(!valid[tokenAddress], "Token not already be valid");
     tokens.push(tokenAddress);
     valid[tokenAddress] = true;
   }
 
-  modifier onlyThis() {
-    if (msg.sender == address(this)) {
+  modifier onlyGovernor() {
+    if (msg.sender == governor) {
       _;
     }
   }
