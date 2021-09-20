287407efa2c7efa9078349a36852748588b6d54a
Drew Stone <drewstone329@gmail.com>
2021-09-20 11:57:45 -0400

    Add link anchor script

---
+++ scripts/evm/linkAnchors.ts
@@ -0,0 +1,72 @@
+require('dotenv').config();
+import { ethers } from 'ethers';
+
+import { WEBB__factory } from '../../typechain/factories/WEBB__factory';
+import { WEBBAnchor2__factory } from '../../typechain/factories/WEBBAnchor2__factory';
+import { Bridge__factory } from '../../typechain/factories/Bridge__factory';
+import helpers from '../../test/helpers';
+
+let provider = new ethers.providers.JsonRpcProvider(`${process.env.ENDPOINT}`);
+
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+
+async function getAllContracts(originWallet, destWallet) {
+  return {
+    chain1Anchor: WEBBAnchor2__factory.connect(process.env.CHAIN_1_WEBBAnchor, originWallet.provider),
+    chain1WebbToken: WEBB__factory.connect(process.env.CHAIN_1_WEBB, originWallet.provider),
+    chain1Bridge: Bridge__factory.connect(process.env.CHAIN_1_Bridge, originWallet.provider),
+    chain2Anchor: WEBBAnchor2__factory.connect(process.env.CHAIN_2_WEBBAnchor, destWallet.provider),
+    chain2WebbToken: WEBB__factory.connect(process.env.CHAIN_2_WEBB, destWallet.provider),
+    chain2Bridge: Bridge__factory.connect(process.env.CHAIN_2_Bridge, destWallet.provider),
+  };
+}
+
+async function proposeAndExecute({
+  originProvider,
+  originAnchor,
+  destAnchor,
+  destBridge,
+  originWallet,
+}) {
+  const originChainID = await originWallet.getChainId();
+  const originBlockHeight = await originProvider.getBlockNumber();
+  const originMerkleRoot = await originAnchor.getLastRoot();
+  // create correct update proposal data for the deposit on origin chain
+  const updateData = helpers.createUpdateProposalData(originChainID, originBlockHeight, originMerkleRoot);
+  const dataHash = ethers.utils.keccak256(destAnchor.address + updateData.substr(2));
+  
+  const resourceId = helpers.createResourceID(originAnchor.address, originChainID);
+  await destBridge.voteProposal(
+    originChainID,
+    '1',
+    resourceId,
+    dataHash,
+  );
+
+  await destBridge.executeProposal(
+    originChainID,
+    '1',
+    updateData,
+    resourceId,
+  );
+  
+  console.log('origin merkle root', originMerkleRoot);
+  console.log('destination neighbor roots', await destAnchor.getNeighborRoots());
+}
+
+async function run() {
+  const chain1Provider = new ethers.providers.JsonRpcProvider(`${process.env.CHAIN_1_ENDPOINT}`);
+  const chain2Provider = new ethers.providers.JsonRpcProvider(`${process.env.CHAIN_2_ENDPOINT}`);
+  const chain1Wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, chain1Provider);
+  const chain2wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, chain2Provider);
+  const options = await getAllContracts(chain1Wallet, chain2wallet);
+  await proposeAndExecute({
+    originProvider: chain1Provider,
+    originAnchor: options.chain1Anchor,
+    destAnchor: options.chain2Anchor,
+    destBridge: options.chain2Bridge,
+    originWallet: chain1Wallet,
+  });
+}
+
+run();
--- scripts/evm/tokens/inspectWebbInstance.ts
+++ scripts/evm/tokens/inspectWebbInstance.ts
@@ -1,25 +1,18 @@
 require('dotenv').config();
-const helpers = require('../../test/helpers');
 import { ethers } from 'ethers';
 
-import { WEBB__factory } from '../../typechain/factories/WEBB__factory';
-import { WEBBAnchor2__factory } from '../../typechain/factories/WEBBAnchor2__factory';
+import { WEBB__factory } from '../../../typechain/factories/WEBB__factory';
+import { WEBBAnchor2__factory } from '../../../typechain/factories/WEBBAnchor2__factory';
 
 let provider = new ethers.providers.JsonRpcProvider(`${process.env.ENDPOINT}`);
 
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
 
 async function run() {
-  const denomination = ethers.BigNumber.from('100000000000000000');
-  // WARNING: ENSURE THIS MATCHES CIRCUIT HEIGHT
-  const merkleTreeHeight = 30;
-
-  const chainId = await wallet.getChainId();
   // @ts-ignore
   const WEBB = WEBB__factory.connect(process.env.WEBB, wallet.provider);
   // @ts-ignore
   const WEBBAnchor = WEBBAnchor2__factory.connect(process.env.WEBBAnchor, wallet.provider);
-  console.log(process.env.WEBBAnchor);
   const root = await WEBBAnchor.getLastRoot();
   console.log(root);
 }
