34e7f240339a6f9b99eb56bd1e591614958f9e13
Drew Stone <drewstone329@gmail.com>
2021-10-13 14:59:43 -0400

    Add new set membership gadget

--- scripts/bash/compile_circom.sh
+++ scripts/bash/compile_circom.sh
@@ -10,40 +10,40 @@ mkdir -p artifacts/circuits/bridge
 #   --sym artifacts/circuits/anchor/withdraw_30.sym
 # echo "Done!\n"
 
-echo "Compiling Webb style Poseidon bridge 2 withdrawal circuit..."
-circom circuits/test/poseidon_bridge_2.circom \
-  --r1cs artifacts/circuits/bridge/poseidon_bridge_2.r1cs \
-  --wasm artifacts/circuits/bridge/poseidon_bridge_2.wasm \
-  --sym artifacts/circuits/bridge/poseidon_bridge_2.sym
-echo "Done!\n"
+# echo "Compiling Webb style Poseidon bridge 2 withdrawal circuit..."
+# circom circuits/test/poseidon_bridge_2.circom \
+#   --r1cs artifacts/circuits/bridge/poseidon_bridge_2.r1cs \
+#   --wasm artifacts/circuits/bridge/poseidon_bridge_2.wasm \
+#   --sym artifacts/circuits/bridge/poseidon_bridge_2.sym
+# echo "Done!\n"
 
-echo "Compiling Webb style Poseidon bridge 3 withdrawal circuit..."
-circom circuits/test/poseidon_bridge_3.circom \
-  --r1cs artifacts/circuits/bridge/poseidon_bridge_3.r1cs \
-  --wasm artifacts/circuits/bridge/poseidon_bridge_3.wasm \
-  --sym artifacts/circuits/bridge/poseidon_bridge_3.sym
-echo "Done!\n"
+# echo "Compiling Webb style Poseidon bridge 3 withdrawal circuit..."
+# circom circuits/test/poseidon_bridge_3.circom \
+#   --r1cs artifacts/circuits/bridge/poseidon_bridge_3.r1cs \
+#   --wasm artifacts/circuits/bridge/poseidon_bridge_3.wasm \
+#   --sym artifacts/circuits/bridge/poseidon_bridge_3.sym
+# echo "Done!\n"
 
-echo "Compiling Webb style Poseidon bridge 4 withdrawal circuit..."
-circom circuits/test/poseidon_bridge_4.circom \
-  --r1cs artifacts/circuits/bridge/poseidon_bridge_4.r1cs \
-  --wasm artifacts/circuits/bridge/poseidon_bridge_4.wasm \
-  --sym artifacts/circuits/bridge/poseidon_bridge_4.sym
-echo "Done!\n"
+# echo "Compiling Webb style Poseidon bridge 4 withdrawal circuit..."
+# circom circuits/test/poseidon_bridge_4.circom \
+#   --r1cs artifacts/circuits/bridge/poseidon_bridge_4.r1cs \
+#   --wasm artifacts/circuits/bridge/poseidon_bridge_4.wasm \
+#   --sym artifacts/circuits/bridge/poseidon_bridge_4.sym
+# echo "Done!\n"
 
-echo "Compiling Webb style Poseidon bridge 5 withdrawal circuit..."
-circom circuits/test/poseidon_bridge_5.circom \
-  --r1cs artifacts/circuits/bridge/poseidon_bridge_5.r1cs \
-  --wasm artifacts/circuits/bridge/poseidon_bridge_5.wasm \
-  --sym artifacts/circuits/bridge/poseidon_bridge_5.sym
-echo "Done!\n"
+# echo "Compiling Webb style Poseidon bridge 5 withdrawal circuit..."
+# circom circuits/test/poseidon_bridge_5.circom \
+#   --r1cs artifacts/circuits/bridge/poseidon_bridge_5.r1cs \
+#   --wasm artifacts/circuits/bridge/poseidon_bridge_5.wasm \
+#   --sym artifacts/circuits/bridge/poseidon_bridge_5.sym
+# echo "Done!\n"
 
-echo "Compiling Webb style Poseidon bridge 6 withdrawal circuit..."
-circom circuits/test/poseidon_bridge_6.circom \
-  --r1cs artifacts/circuits/bridge/poseidon_bridge_6.r1cs \
-  --wasm artifacts/circuits/bridge/poseidon_bridge_6.wasm \
-  --sym artifacts/circuits/bridge/poseidon_bridge_6.sym
-echo "Done!\n"
+# echo "Compiling Webb style Poseidon bridge 6 withdrawal circuit..."
+# circom circuits/test/poseidon_bridge_6.circom \
+#   --r1cs artifacts/circuits/bridge/poseidon_bridge_6.r1cs \
+#   --wasm artifacts/circuits/bridge/poseidon_bridge_6.wasm \
+#   --sym artifacts/circuits/bridge/poseidon_bridge_6.sym
+# echo "Done!\n"
 
 # echo "Compiling poseidon preimage circuit..."
 # circom circuits/test/poseidon_preimage.circom \
@@ -59,3 +59,9 @@ echo "Done!\n"
 #   --sym artifacts/circuits/poseidon_preimage_3.sym
 # echo "Done!"
 
+echo "Compiling Set membership of length 5 circuit..."
+circom circuits/test/set_membership_5.circom \
+  --r1cs artifacts/circuits/bridge/set_membership_5.r1cs \
+  --wasm artifacts/circuits/bridge/set_membership_5.wasm \
+  --sym artifacts/circuits/bridge/set_membership_5.sym
+echo "Done!\n"
