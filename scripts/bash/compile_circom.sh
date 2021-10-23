458507442da1686eb40d64dee4b9e9f2e18c593f
Adam Spiers <blockchain@adamspiers.org>
2021-10-23 17:02:24 +0100

    Add missing -e to echo to fix newline output

--- scripts/bash/compile_circom.sh
+++ scripts/bash/compile_circom.sh
@@ -13,7 +13,7 @@ circom circuits/test/anchor_withdraw_30.circom \
   --r1cs artifacts/circuits/anchor/withdraw_30.r1cs \
   --wasm artifacts/circuits/anchor/withdraw_30.wasm \
   --sym artifacts/circuits/anchor/withdraw_30.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 ###
 # WEBB BRIDGES
@@ -24,35 +24,35 @@ circom circuits/test/poseidon_bridge_2.circom \
   --r1cs artifacts/circuits/bridge/poseidon_bridge_2.r1cs \
   --wasm artifacts/circuits/bridge/poseidon_bridge_2.wasm \
   --sym artifacts/circuits/bridge/poseidon_bridge_2.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 echo "Compiling Webb style Poseidon bridge 3 withdrawal circuit..."
 circom circuits/test/poseidon_bridge_3.circom \
   --r1cs artifacts/circuits/bridge/poseidon_bridge_3.r1cs \
   --wasm artifacts/circuits/bridge/poseidon_bridge_3.wasm \
   --sym artifacts/circuits/bridge/poseidon_bridge_3.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 echo "Compiling Webb style Poseidon bridge 4 withdrawal circuit..."
 circom circuits/test/poseidon_bridge_4.circom \
   --r1cs artifacts/circuits/bridge/poseidon_bridge_4.r1cs \
   --wasm artifacts/circuits/bridge/poseidon_bridge_4.wasm \
   --sym artifacts/circuits/bridge/poseidon_bridge_4.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 echo "Compiling Webb style Poseidon bridge 5 withdrawal circuit..."
 circom circuits/test/poseidon_bridge_5.circom \
   --r1cs artifacts/circuits/bridge/poseidon_bridge_5.r1cs \
   --wasm artifacts/circuits/bridge/poseidon_bridge_5.wasm \
   --sym artifacts/circuits/bridge/poseidon_bridge_5.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 echo "Compiling Webb style Poseidon bridge 6 withdrawal circuit..."
 circom circuits/test/poseidon_bridge_6.circom \
   --r1cs artifacts/circuits/bridge/poseidon_bridge_6.r1cs \
   --wasm artifacts/circuits/bridge/poseidon_bridge_6.wasm \
   --sym artifacts/circuits/bridge/poseidon_bridge_6.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 ###
 # POSEIDON PREIMAGES
@@ -63,14 +63,14 @@ circom circuits/test/poseidon_preimage.circom \
   --r1cs artifacts/circuits/poseidon_preimage.r1cs \
   --wasm artifacts/circuits/poseidon_preimage.wasm \
   --sym artifacts/circuits/poseidon_preimage.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 echo "Compiling poseidon3 preimage circuit..."
 circom circuits/test/poseidon_preimage_3.circom \
   --r1cs artifacts/circuits/poseidon_preimage_3.r1cs \
   --wasm artifacts/circuits/poseidon_preimage_3.wasm \
   --sym artifacts/circuits/poseidon_preimage_3.sym
-echo "Done!"
+echo -e "Done!\n"
 
 ###
 # SET MEMBERSHIP
@@ -81,7 +81,7 @@ circom circuits/test/set_membership_5.circom \
   --r1cs artifacts/circuits/bridge/set_membership_5.r1cs \
   --wasm artifacts/circuits/bridge/set_membership_5.wasm \
   --sym artifacts/circuits/bridge/set_membership_5.sym
-echo "Done!\n"
+echo -e "Done!\n"
 
 ###
 # WEBB SEMPAHORES
@@ -91,4 +91,4 @@ circom circuits/test/semaphore_bridge_2.circom \
   --r1cs artifacts/circuits/semaphore/semaphore_bridge_2.r1cs \
   --wasm artifacts/circuits/semaphore/semaphore_bridge_2.wasm \
   --sym artifacts/circuits/semaphore/semaphore_bridge_2.sym
-echo "Done!\n"
+echo -e "Done!\n"
