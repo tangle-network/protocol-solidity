dd8423c45e10524a37aae2d401945fc5eb8374c0
Drew Stone <drewstone329@gmail.com>
2021-10-25 02:11:30 +0100

    Fixes 3 sided anchor test

--- test/anchor/anchor.test.ts
+++ test/anchor/anchor.test.ts
@@ -833,7 +833,7 @@ describe('Anchor for 3 max edges', () => {
     }
   })
 
-  it.skip('should withdraw successfully', async () => {
+  it('should withdraw successfully', async () => {
     const signers = await ethers.getSigners();
     const sender = signers[0];
     const relayer = signers[1];
