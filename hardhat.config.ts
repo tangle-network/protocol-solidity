5011686377ef520efa3b7e3984ac53806a37dbb4
Nathan Barnavon <nepoche@yahoo.com>
2021-11-19 14:17:25 -0400

    Change hasher overwriteArtifact location to occur before generating types

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,23 @@ import "hardhat-artifactor";
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers'
 import "@nomiclabs/hardhat-truffle5";
+import { subtask } from 'hardhat/config'
+
+const poseidonGenContract = require('circomlibjs/src/poseidon_gencontract.js');
+
+const buildPoseidon = async (numInputs: number) => {
+  //@ts-ignore
+  await overwriteArtifact(`PoseidonT${numInputs + 1}`, poseidonGenContract.createCode(numInputs));
+}
+
+subtask('typechain-generate-types',
+  async (taskArgs, hre, runSuper) => {
+
+    // overwrite the artifact before generating types
+    await buildPoseidon(2);
+    await runSuper();
+  }
+)
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
