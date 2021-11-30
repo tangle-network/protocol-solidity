2341af98d2ff609b708acf26d2f9d7143a4e148a
Akilesh Tangella <saiakilesh@gmail.com>
2021-11-30 11:07:56 -0500

    anchor trees is compiling

--- contracts/anchor_trees/AnchorTrees.sol
+++ contracts/anchor_trees/AnchorTrees.sol
@@ -11,7 +11,6 @@ import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 contract AnchorTrees is Initializable {
   address public immutable governance;
   //Roots Stuff
-  mapping(uint256 => bytes32) public roots;
   uint32 public constant ROOT_HISTORY_SIZE = 5;
   uint32 public currentDepositRootIndex = 0;
   uint32 public currentWithdrawalRootIndex = 0;
@@ -65,8 +64,8 @@ contract AnchorTrees is Initializable {
   mapping(uint256 => uint32) public currentNeighborWithdrawalRootIndex;
   //End Edge Information
 
-  event DepositData(address instance, bytes32 indexed hash, uint256 block, uint256 index);
-  event WithdrawalData(address instance, bytes32 indexed hash, uint256 block, uint256 index);
+  event DepositData(address instance, bytes32 indexed hash, uint256 blockTimestamp, uint256 index);
+  event WithdrawalData(address instance, bytes32 indexed hash, uint256 blockTimestamp, uint256 index);
   event VerifierUpdated(address newVerifier);
   event ProxyUpdated(address newProxy);
   event EdgeAddition(uint256 chainID, uint256 latestLeafIndex, bytes32 depositRoot, bytes32 withdrawalRoot);
@@ -75,7 +74,7 @@ contract AnchorTrees is Initializable {
   struct TreeLeaf {
     bytes32 hash;
     address instance;
-    uint32 block;
+    uint256 blockTimestamp;
   }
 
   modifier onlyAnchorProxy {
@@ -140,16 +139,16 @@ contract AnchorTrees is Initializable {
   /// @dev Queue a new deposit data to be inserted into a merkle tree
   function registerDeposit(address _instance, bytes32 _commitment) public onlyAnchorProxy {
     uint256 _depositsLength = depositsLength;
-    deposits[_depositsLength] = keccak256(abi.encode(_instance, _commitment, blockNumber()));
-    emit DepositData(_instance, _commitment, blockNumber(), _depositsLength);
+    deposits[_depositsLength] = keccak256(abi.encode(_instance, _commitment, blockTimestamp()));
+    emit DepositData(_instance, _commitment, blockTimestamp(), _depositsLength);
     depositsLength = _depositsLength + 1;
   }
 
   /// @dev Queue a new withdrawal data to be inserted into a merkle tree
   function registerWithdrawal(address _instance, bytes32 _nullifierHash) public onlyAnchorProxy {
     uint256 _withdrawalsLength = withdrawalsLength;
-    withdrawals[_withdrawalsLength] = keccak256(abi.encode(_instance, _nullifierHash, blockNumber()));
-    emit WithdrawalData(_instance, _nullifierHash, blockNumber(), _withdrawalsLength);
+    withdrawals[_withdrawalsLength] = keccak256(abi.encode(_instance, _nullifierHash, blockTimestamp()));
+    emit WithdrawalData(_instance, _nullifierHash, blockTimestamp(), _withdrawalsLength);
     withdrawalsLength = _withdrawalsLength + 1;
   }
 
@@ -169,7 +168,7 @@ contract AnchorTrees is Initializable {
     TreeLeaf[CHUNK_SIZE] calldata _events
   ) public {
     uint256 offset = lastProcessedDepositLeaf;
-    require(_currentRoot == depositRoot, "Proposed deposit root is invalid");
+    require(_currentRoot == depositRoots[currentDepositRootIndex], "Proposed deposit root is invalid");
     require(_pathIndices == offset >> CHUNK_TREE_HEIGHT, "Incorrect deposit insert index");
 
     bytes memory data = new bytes(BYTES_SIZE);
@@ -179,20 +178,20 @@ contract AnchorTrees is Initializable {
       mstore(add(data, 0x20), _currentRoot)
     }
     for (uint256 i = 0; i < CHUNK_SIZE; i++) {
-      (bytes32 hash, address instance, uint32 blockNumber) = (_events[i].hash, _events[i].instance, _events[i].block);
-      bytes32 leafHash = keccak256(abi.encode(instance, hash, blockNumber));
+      (bytes32 hash, address instance, uint256 blockTimestamp) = (_events[i].hash, _events[i].instance, _events[i].blockTimestamp);
+      bytes32 leafHash = keccak256(abi.encode(instance, hash, blockTimestamp));
       bytes32 deposit = offset + i >= depositsV1Length ? deposits[offset + i] : anchorTreesV1.deposits(offset + i);
       require(leafHash == deposit, "Incorrect deposit");
       assembly {
         let itemOffset := add(data, mul(ITEM_SIZE, i))
-        mstore(add(itemOffset, 0x7c), blockNumber)
-        mstore(add(itemOffset, 0x78), instance)
-        mstore(add(itemOffset, 0x64), hash)
+        mstore(add(itemOffset, 0x7c), blockTimestamp)
+        mstore(add(itemOffset, 0x5c), instance)
+        mstore(add(itemOffset, 0x48), hash)
       }
       if (offset + i >= depositsV1Length) {
         delete deposits[offset + i];
       } else {
-        emit DepositData(instance, hash, blockNumber, offset + i);
+        emit DepositData(instance, hash, blockTimestamp, offset + i);
       }
     }
 
@@ -200,8 +199,9 @@ contract AnchorTrees is Initializable {
     require(argsHash == uint256(_argsHash), "Invalid args hash");
     require(treeUpdateVerifier.verifyProof(_proof, [argsHash]), "Invalid deposit tree update proof");
 
-    previousDepositRoot = _currentRoot;
-    depositRoot = _newRoot;
+    uint32 newDepositRootIndex = (currentDepositRootIndex + 1) % ROOT_HISTORY_SIZE;
+    currentDepositRootIndex = newDepositRootIndex;
+    depositRoots[newDepositRootIndex] = _newRoot;
     lastProcessedDepositLeaf = offset + CHUNK_SIZE;
   }
 
@@ -221,7 +221,7 @@ contract AnchorTrees is Initializable {
     TreeLeaf[CHUNK_SIZE] calldata _events
   ) public {
     uint256 offset = lastProcessedWithdrawalLeaf;
-    require(_currentRoot == withdrawalRoot, "Proposed withdrawal root is invalid");
+    require(_currentRoot == withdrawalRoots[currentWithdrawalRootIndex], "Proposed withdrawal root is invalid");
     require(_pathIndices == offset >> CHUNK_TREE_HEIGHT, "Incorrect withdrawal insert index");
 
     bytes memory data = new bytes(BYTES_SIZE);
@@ -231,20 +231,20 @@ contract AnchorTrees is Initializable {
       mstore(add(data, 0x20), _currentRoot)
     }
     for (uint256 i = 0; i < CHUNK_SIZE; i++) {
-      (bytes32 hash, address instance, uint32 blockNumber) = (_events[i].hash, _events[i].instance, _events[i].block);
-      bytes32 leafHash = keccak256(abi.encode(instance, hash, blockNumber));
+      (bytes32 hash, address instance, uint256 blockTimestamp) = (_events[i].hash, _events[i].instance, _events[i].blockTimestamp);
+      bytes32 leafHash = keccak256(abi.encode(instance, hash, blockTimestamp));
       bytes32 withdrawal = offset + i >= withdrawalsV1Length ? withdrawals[offset + i] : anchorTreesV1.withdrawals(offset + i);
       require(leafHash == withdrawal, "Incorrect withdrawal");
       assembly {
         let itemOffset := add(data, mul(ITEM_SIZE, i))
-        mstore(add(itemOffset, 0x7c), blockNumber)
-        mstore(add(itemOffset, 0x78), instance)
-        mstore(add(itemOffset, 0x64), hash)
+        mstore(add(itemOffset, 0x7c), blockTimestamp)
+        mstore(add(itemOffset, 0x5c), instance)
+        mstore(add(itemOffset, 0x48), hash)
       }
       if (offset + i >= withdrawalsV1Length) {
         delete withdrawals[offset + i];
       } else {
-        emit WithdrawalData(instance, hash, blockNumber, offset + i);
+        emit WithdrawalData(instance, hash, blockTimestamp, offset + i);
       }
     }
 
@@ -252,41 +252,66 @@ contract AnchorTrees is Initializable {
     require(argsHash == uint256(_argsHash), "Invalid args hash");
     require(treeUpdateVerifier.verifyProof(_proof, [argsHash]), "Invalid withdrawal tree update proof");
 
-    previousWithdrawalRoot = _currentRoot;
-    withdrawalRoot = _newRoot;
+    uint32 newWithdrawalRootIndex = (currentWithdrawalRootIndex + 1) % ROOT_HISTORY_SIZE;
+    currentWithdrawalRootIndex = newWithdrawalRootIndex;
+    withdrawalRoots[newWithdrawalRootIndex] = _newRoot;
     lastProcessedWithdrawalLeaf = offset + CHUNK_SIZE;
   }
 
-    /**
+  /**
     @dev Whether the root is present in the root history
   */
-  function isKnownRoot(bytes32 _root) public view returns (bool) {
-    if (_root == 0) {
+  function isKnownDepositRoot(bytes32 _depositRoot) public view returns (bool) {
+    if (_depositRoot == 0) {
       return false;
     }
-    uint32 _currentRootIndex = currentRootIndex;
-    uint32 i = _currentRootIndex;
+    uint32 _currentDepositRootIndex = currentDepositRootIndex;
+    uint32 i = _currentDepositRootIndex;
     do {
-      if (_root == roots[i]) {
+      if (_depositRoot == depositRoots[i]) {
         return true;
       }
       if (i == 0) {
         i = ROOT_HISTORY_SIZE;
       }
       i--;
-    } while (i != _currentRootIndex);
+    } while (i != _currentDepositRootIndex);
+    return false;
+  }
+
+  /**
+    @dev Whether the root is present in the root history
+  */
+  function isKnownWithdrawalRoot(bytes32 _withdrawalRoot) public view returns (bool) {
+    if (_withdrawalRoot == 0) {
+      return false;
+    }
+    uint32 _currentWithdrawalRootIndex = currentWithdrawalRootIndex;
+    uint32 i = _currentWithdrawalRootIndex;
+    do {
+      if (_withdrawalRoot == withdrawalRoots[i]) {
+        return true;
+      }
+      if (i == 0) {
+        i = ROOT_HISTORY_SIZE;
+      }
+      i--;
+    } while (i != _currentWithdrawalRootIndex);
     return false;
   }
 
     /** @dev */
-  function getLatestNeighborRoots() public view returns (bytes32[] memory roots) {
-    roots = new bytes32[](maxEdges);
+  function getLatestNeighborRoots() public view returns (bytes32[] memory depositRoots, bytes32[] memory withdrawalRoots) {
+    depositRoots = new bytes32[](maxEdges);
+    withdrawalRoots = new bytes32[](maxEdges);
     for (uint256 i = 0; i < maxEdges; i++) {
       if (edgeList.length >= i + 1) {
-        roots[i] = edgeList[i].root;
+        depositRoots[i] = edgeList[i].depositRoot;
+       withdrawalRoots[i] = edgeList[i].withdrawalRoot;
       } else {
         // merkle tree height for zeros
-        roots[i] = 0; //was previously zeros(levels);
+        depositRoots[i] = bytes32(0x00); //was previously zeros(levels);
+        withdrawalRoots[i] = bytes32(0x00); //was previously zeros(levels);
       }
     }
   }
@@ -296,43 +321,56 @@ contract AnchorTrees is Initializable {
     if (_depositRoot == 0) {
       return false;
     }
-    uint32 _currentRootIndex = currentNeighborRootIndex[neighborChainID][0];
-    uint32 i = _currentRootIndex;
+    uint32 _currentDepositRootIndex = currentNeighborDepositRootIndex[neighborChainID];
+    uint32 i = _currentDepositRootIndex;
     do {
-      if (_depositRoot == neighborRoots[neighborChainID][i][0]) {
+      if (_depositRoot == neighborDepositRoots[neighborChainID][i]) {
         return true;
       }
       if (i == 0) {
         i = ROOT_HISTORY_SIZE;
       }
       i--;
-    } while (i != _currentRootIndex);
+    } while (i != _currentDepositRootIndex);
     return false;
   }
 
-    /** @dev */
+ /** @dev */
   function isKnownNeighborWithdrawalRoot(uint256 neighborChainID, bytes32 _withdrawalRoot) public view returns (bool) {
     if (_withdrawalRoot == 0) {
       return false;
     }
-    uint32 _currentRootIndex = currentNeighborRootIndex[neighborChainID][1];
-    uint32 i = _currentRootIndex;
+    uint32 _currentWithdrawalRootIndex = currentNeighborDepositRootIndex[neighborChainID];
+    uint32 i = _currentWithdrawalRootIndex;
     do {
-      if (_withdrawalRoot == neighborRoots[neighborChainID][i][1]) {
+      if (_withdrawalRoot == neighborWithdrawalRoots[neighborChainID][i]) {
         return true;
       }
       if (i == 0) {
         i = ROOT_HISTORY_SIZE;
       }
       i--;
-    } while (i != _currentRootIndex);
+    } while (i != _currentWithdrawalRootIndex);
     return false;
   }
 
-  function validateRoots(bytes32 _depositRoot, bytes32 _withdrawalRoot) public view {
-    require(_depositRoot == depositRoot || _depositRoot == previousDepositRoot, "Incorrect deposit tree root");
-    require(_withdrawalRoot == withdrawalRoot || _withdrawalRoot == previousWithdrawalRoot, "Incorrect withdrawal tree root");
+  function validateRoots(bytes32[] memory _depositRoots, bytes32[] memory _withdrawalRoots) public view returns (bool) {
+    require(isKnownDepositRoot(_depositRoots[0]), "Cannot find your deposit merkle root");
+    require(isKnownWithdrawalRoot(_withdrawalRoots[0]), "Cannot find your withdrawal merkle root");
+    require(_depositRoots.length == maxEdges + 1, "Incorrect deposit root array length");
+    require(_withdrawalRoots.length == maxEdges + 1, "Incorrect withdrawal root array length");
+    for (uint i = 0; i < edgeList.length; i++) {
+      Edge memory _edge = edgeList[i];
+      require(isKnownNeighborDepositRoot(_edge.chainID, _depositRoots[i+1]), "deposit Neighbor root not found");
+      require(isKnownNeighborWithdrawalRoot(_edge.chainID, _withdrawalRoots[i+1]), "withdrawal Neighbor root not found");
+    }
+    return true;
   }
+
+  // function validateRoots(bytes32 _depositRoot, bytes32 _withdrawalRoot) public view {
+  //   require(_depositRoot == depositRoot || _depositRoot == previousDepositRoot, "Incorrect deposit tree root");
+  //   require(_withdrawalRoot == withdrawalRoot || _withdrawalRoot == previousWithdrawalRoot, "Incorrect withdrawal tree root");
+  // }
 
   /// @dev There is no array length getter for deposit and withdrawal arrays
   /// in the previous contract, so we have to find them length manually.
@@ -383,7 +421,7 @@ contract AnchorTrees is Initializable {
     emit VerifierUpdated(address(_treeUpdateVerifier));
   }
 
-  function blockNumber() public view virtual returns (uint256) {
-    return block.number;
+  function blockTimestamp() public view virtual returns (uint256) {
+    return block.timestamp;
   }
 }
