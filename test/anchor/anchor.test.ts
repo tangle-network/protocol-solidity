5505eeaf308da9d7900575f3df441893fd628529
Akilesh Tangella <saiakilesh@gmail.com>
2022-01-27 10:33:59 -0500

    wrap fee test passing

--- test/anchor/anchor.test.ts
+++ test/anchor/anchor.test.ts
@@ -810,7 +810,7 @@ describe('Anchor for 2 max edges', () => {
       const balUnwrappedTokenBeforeWithdrawSender = await token.balanceOf(sender.address);
 
       const newAnchor = await Anchor.connect(wrappedAnchor.contract.address, zkComponents, wallet);
-      await TruffleAssert.passes(newAnchor.withdrawAndUnwrap(deposit, 31337, index, sender.address, signers[1].address, bigInt(0), bigInt(0), token.address));
+      await TruffleAssert.passes(newAnchor.withdrawAndUnwrap(deposit, getChainIdType(31337), index, sender.address, signers[1].address, bigInt(0), bigInt(0), token.address));
       const balWrappedTokenAfterWithdrawAnchor = await wrappedToken.balanceOf(wrappedAnchor.contract.address);
       assert.strictEqual(balWrappedTokenAfterWithdrawAnchor.toString(), '0');
 
