2b20d5996dcda571e9629f86829534c6b2360f8f
nepoche <nepoche@yahoo.com>
2022-03-30 20:28:34 -0400

    Remove maci crypto from protocol-solidity

--- packages/utils/src/PoseidonHasher.ts
+++ packages/utils/src/PoseidonHasher.ts
@@ -1,15 +1,30 @@
 const circomlibjs = require('circomlibjs');
-const maci = require('maci-crypto');
 
-class PoseidonHasher {
-  hash(level: any, left: any, right: any) {
-    return maci.hashLeftRight(BigInt(left), BigInt(right)).toString()
+const hashLeftRight = (left: bigint, right: bigint) => {
+  return circomlibjs.poseidon([left, right]);
+};
+
+function poseidonHash3 (inputs: any[]) {
+  if (inputs.length !== 3) {
+    throw new Error('panic');
   }
 
-  hash3(inputs: any) {
-    if (inputs.length !== 3) throw new Error('panic');
+  return circomlibjs.poseidon(inputs);
+}
+
+class PoseidonHasher {
+  hash (level: any, left: any, right: any) {
+    return hashLeftRight(BigInt(left), BigInt(right)).toString();
+  }
+
+  hash3 (inputs: any) {
+    if (inputs.length !== 3) {
+      throw new Error('panic');
+    }
+
     return circomlibjs.poseidon(inputs);
   }
 }
+
 
 export { PoseidonHasher };
