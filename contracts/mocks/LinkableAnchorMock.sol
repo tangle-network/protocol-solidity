ea7051691d37a3ac05fbccb68dff7eab0e547401
Drew Stone <drewstone329@gmail.com>
2022-08-11 10:32:44 -0400

    Spacing to tabs

--- contracts/mocks/LinkableAnchorMock.sol
+++ contracts/mocks/LinkableAnchorMock.sol
@@ -9,36 +9,36 @@ import "../anchors/LinkableAnchor.sol";
 import "../interfaces/IAnchorVerifier.sol";
 
 contract LinkableAnchorMock is LinkableAnchor {
-    constructor(
+	constructor(
 		address _handler,
 		IAnchorVerifier _verifier,
 		IPoseidonT3 _hasher,
 		uint32 _merkleTreeHeight,
 		uint8 _maxEdges
 	) LinkableAnchor(_handler, _hasher, _merkleTreeHeight, _maxEdges) {
-        require(address(_verifier) != address(0), "Verifier address cannot be 0");
-        return;
+		require(address(_verifier) != address(0), "Verifier address cannot be 0");
+		return;
 	}
 
 	function setHandler(address _handler, uint32 _nonce) override external onlyHandler {
-        handler = _handler;
-        proposalNonce = _nonce;
-        return;
-    }
+		handler = _handler;
+		proposalNonce = _nonce;
+		return;
+	}
 
 	function setVerifier(address _verifier, uint32 _nonce) override external onlyHandler {
-        _verifier = _verifier;
-        proposalNonce = _nonce;
-        return;
-    }
+		_verifier = _verifier;
+		proposalNonce = _nonce;
+		return;
+	}
 
 	function configureMinimalWithdrawalLimit(uint256 _minimalWithdrawalAmount, uint32 _nonce) view override external onlyHandler {
-        _minimalWithdrawalAmount + _minimalWithdrawalAmount;
-        return;
-    }
+		_minimalWithdrawalAmount + _minimalWithdrawalAmount;
+		return;
+	}
 
 	function configureMaximumDepositLimit(uint256 _maximumDepositAmount, uint32 _nonce) view override external onlyHandler {
-        _maximumDepositAmount + _maximumDepositAmount;
-        return;
-    }
+		_maximumDepositAmount + _maximumDepositAmount;
+		return;
+	}
 }
