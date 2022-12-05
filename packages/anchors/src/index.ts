8469911fb99482cfc3e4a9c0fe894d045dbd31df
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-12-05 20:24:51 -0300

    chore: add UtxoUtils to `packages/anchors/src/index.ts` to use in vbridge

--- packages/anchors/src/index.ts
+++ packages/anchors/src/index.ts
@@ -1,6 +1,7 @@
 export { AnchorHandler } from './AnchorHandler';
 export { VAnchor } from './VAnchor';
 export { VAnchorForest } from './VAnchorForest';
+export { UtxoUtils } from './UtxoUtils';
 export { ChainalysisVAnchor } from './ChainalysisVAnchor';
 export { IdentityVAnchor } from './IdentityVAnchor';
 export { OpenVAnchor } from './OpenVAnchor';
