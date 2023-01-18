d6759a6da704caa34069823b04298c5cba328593
drewstone <drewstone329@gmail.com>
2023-01-18 12:23:28 -0500

    Adds helper methods to vanchor (#235)

--- packages/contracts/contracts/mocks/BatchTreeMock.sol
+++ packages/contracts/contracts/mocks/BatchTreeMock.sol
@@ -6,9 +6,13 @@ import "../hashers/IHasher.sol";
 import "../interfaces/verifiers/IBatchVerifier.sol";
 
 contract BatchMerkleTreeMock is BatchMerkleTree {
-	constructor(uint32 _levels, IHasher _hasher, IBatchTreeVerifierSelector _treeUpdateVerifier) BatchMerkleTree(_levels, _hasher, _treeUpdateVerifier) {}
+	constructor(
+		uint32 _levels,
+		IHasher _hasher,
+		IBatchTreeVerifierSelector _treeUpdateVerifier
+	) BatchMerkleTree(_levels, _hasher, _treeUpdateVerifier) {}
 
 	function registerInsertion(address _instance, bytes32 _commitment) public {
-        _registerInsertion(_instance, _commitment);
+		_registerInsertion(_instance, _commitment);
 	}
 }
