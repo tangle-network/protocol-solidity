ceb91b56199bb9c4d897663a530a92f531981d32
Akilesh Tangella <saiakilesh@gmail.com>
2023-03-09 14:33:20 -0500

    some progress

--- packages/contracts/contracts/proxy/MultiAssetVAnchorProxy.sol
+++ packages/contracts/contracts/proxy/MultiAssetVAnchorProxy.sol
@@ -38,19 +38,26 @@ contract MultiAssetVAnchorProxy is Initialized {
 
     mapping(uint256 => bytes32) public RewardUnspentTreeCommitmentMap;
     uint256 nextRewardUnspentTreeCommitmentIndex;
-    uint256 public lastProcessedRewardTreelLeaf;
+    uint256 public lastProcessedRewardUnspentTreelLeaf;
+
+    mapping(uint256 => bytes32) public RewardSpentTreeCommitmentMap;
+    uint256 nextRewardSpentTreeCommitmentIndex;
+    uint256 public lastProcessedRewardSpentTreelLeaf;
 
     IHasher public hasher;
 
     mapping(address => bool) public proxiedMASPs;
 
+    mapping(address => bool) public proxiedRewardTrees;
+
     constructor(IHasher _hasher) public {
         hasher = _hasher;
     }
 
-    function initialize(IMultiAssetVAnchorBatchTree[] memory _proxiedMASPs) public onlyUninitialized {
+    function initialize(IMultiAssetVAnchorBatchTree[] memory _proxiedMASPs, IRewardTrees memory proxiedRewardTrees) public onlyUninitialized {
         for (uint256 i = 0; i < _proxiedMASPs.length; i++) {
             proxiedMASPs[address(_proxiedMASPs[i])] = true;
+            proxiedRewardTrees[address(proxiedRewardTrees)] = true;
         }
     }
 
@@ -115,8 +122,44 @@ contract MultiAssetVAnchorProxy is Initialized {
     }
 
     // TODO: Batch Insert Into Reward Unspent Tree
+    function batchInsertRewardUnspentTree(IRewardTrees proxiedRewardTree, bytes32 _argsHash, bytes32 _currentRoot, bytes32 _newRoot, uint32 _pathIndices, uint8 _batchHeight) public {
+        // Calculate commitment = hash of QueueDepositInfo data
+        require(proxiedRewardTrees[address(proxiedRewardTree)], "Invalid Reward Tree");
+        uint256 _batchSize = 2 ** _batchHeight;
+        bytes32[] memory commitments = new bytes32[](_batchSize);
+        uint _lastProcessedRewardUnspentTreeLeaf = lastProcessedRewardUnspentTreeLeaf;
+        for (uint i = _lastProcessedRewardUnspentTreeLeaf; i < _lastProcessedRewardUnspentTreeLeaf + _batchSize; i++) {
+            commitments[i] = RewardUnspentTreeCommitmentMap[i];
+        } 
+        // Update latestProcessedDepositLeaf
+        lastProcessedRewardUnspentTreeLeaf = _lastProcessedRewardUnspentTreeLeaf + _batchSize;
+        // Call batchInsert function on MASP
+        proxiedRewardTree.batchInsertUnspentTree(_argsHash, _currentRoot, _newRoot, _pathIndices, _batchHeight, commitments);
+    }
+
     // TODO: Queue Reward Spent Tree Commitment
+    function queueRewardSpentTreeCommitment (bytes32 rewardSpentTreeCommitment) public payable {
+        RewardSpentTreeCommitmentMap[nextQueueDepositIndex] = rewardSpentTreeCommitment;
+        // TODO: Emit Event
+        nextRewardSpentTreeCommitmentIndex = nextRewardSpentTreeCommitmentIndex + 1;
+    }
+
     // TODO: Batch Insert Into Reward Spent Tree
+    function batchInsertRewardSpentTree(IRewardTrees proxiedRewardTree, bytes32 _argsHash, bytes32 _currentRoot, bytes32 _newRoot, uint32 _pathIndices, uint8 _batchHeight) public {
+        // Calculate commitment = hash of QueueDepositInfo data
+        require(proxiedRewardTrees[address(proxiedRewardTree)], "Invalid Reward Tree");
+        require(proxiedMASP[msg.sender], "Invalid MASP");
+        uint256 _batchSize = 2 ** _batchHeight;
+        bytes32[] memory commitments = new bytes32[](_batchSize);
+        uint _lastProcessedRewardSpentTreeLeaf = lastProcessedRewardSpentTreeLeaf;
+        for (uint i = _lastProcessedRewardSpentTreeLeaf; i < _lastProcessedRewardSpentTreeLeaf + _batchSize; i++) {
+            commitments[i] = RewardSpentTreeCommitmentMap[i];
+        } 
+        // Update latestProcessedDepositLeaf
+        lastProcessedRewardSpentTreeLeaf = _lastProcessedRewardSpentTreeLeaf + _batchSize;
+        // Call batchInsert function on MASP
+        proxiedRewardTree.batchInsertSpentTree(_argsHash, _currentRoot, _newRoot, _pathIndices, _batchHeight, commitments);
+    }
 
     // TODO: Same logic for NFTs as for Fungible Tokens
     // TODO: Refund Deposit from Queue
