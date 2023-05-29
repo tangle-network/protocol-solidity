0ae1b3fd75fb6c735078db2ac8c29700bec703f1
Drew Stone <drewstone329@gmail.com>
2023-05-29 16:08:15 -0400

    Add base layer treasury-swap

--- packages/contracts/contracts/Treasury.sol
+++ packages/contracts/contracts/Treasury.sol
@@ -8,12 +8,16 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@uniswap/universal-router/contracts/interfaces/IUniversalRouter.sol";
+import "@uniswap/universal-router/contracts/libraries/Commands.sol";
 import "./interfaces/ITreasury.sol";
 import "./utils/ProposalNonceTracker.sol";
 
 contract Treasury is ITreasury, ProposalNonceTracker {
 	using SafeERC20 for IERC20;
 	address treasuryHandler;
+	
+	address UNISWAP_ROUTER;
 
 	event TreasuryHandlerUpdated(address _handler);
 
@@ -27,15 +31,38 @@ contract Treasury is ITreasury, ProposalNonceTracker {
 		_;
 	}
 
+	function swapRescueTokens(
+		address tokenAddress,
+		address payable to,
+		uint256 amountToSwap,
+		uint256 minAmountToReceive,
+		uint32 nonce,
+		address swapToken
+	) external override onlyHandler onlyIncrementingByOne(nonce) {
+		require(to != address(0), "Treasury: Cannot send liquidity to zero address");
+		require(tokenAddress != address(this), "Treasury: Invalid token address specified");
+		require(swapToken != address(0), "Treasury: Invalid swap token address specified");
+		require(amountToSwap > 0, "Treasury: Amount to swap must be greater than 0");
+
+		swap(
+			tokenAddress,
+			swapToken,
+			amountToSwap,
+			minAmountToReceive,
+			to
+		);
+	}
+
 	function rescueTokens(
 		address tokenAddress,
 		address payable to,
 		uint256 amountToRescue,
 		uint32 nonce
 	) external override onlyHandler onlyIncrementingByOne(nonce) {
-		require(to != address(0), "Cannot send liquidity to zero address");
-		require(tokenAddress != address(this), "Cannot rescue wrapped asset");
-
+		require(to != address(0), "Treasury: Cannot send liquidity to zero address");
+		require(tokenAddress != address(this), "Treasury: Invalid token address specified");
+		require(amountToRescue > 0, "Treasury: Amount to rescue must be greater than 0");
+		
 		if (tokenAddress == address(0)) {
 			// Native Ether
 			uint256 ethBalance = address(this).balance;
@@ -65,4 +92,27 @@ contract Treasury is ITreasury, ProposalNonceTracker {
 	}
 
 	receive() external payable {}
+
+	function swap(
+		address _tokenIn,
+		address _tokenOut,
+		uint256 _amountIn,
+		uint256 _minAmountOut,
+		address _recipient
+	) external {
+		bytes memory commands = abi.encodePacked(bytes1(uint8(Commands.V3_SWAP_EXACT_IN)));
+		address[] memory path = new address[](2);
+		path[0] = _tokenIn;
+		path[1] = _tokenOut;
+		bytes[] memory inputs = new bytes[](1);
+		inputs[0] = abi.encode(
+			address(_recipient),
+			_amountIn,
+			_minAmountOut,
+			path,
+			true
+		);
+
+		IUniversalRouter(UNISWAP_ROUTER).execute(commands, inputs);
+	}
 }
--- packages/contracts/contracts/interfaces/ITreasury.sol
+++ packages/contracts/contracts/interfaces/ITreasury.sol
@@ -6,17 +6,17 @@
 pragma solidity ^0.8.18;
 
 /**
-        @title Interface for Treasury contract.
-        @author Webb Technologies.
+	@title Interface for Treasury contract.
+	@author Webb Technologies.
  */
 interface ITreasury {
 	/**
-        @notice Sends an `_amountToRescue` of tokens at `_tokenAddress` from the treasury contract to `_to`
-        @param _tokenAddress Address of the token to rescue.
-        @param _to Address of the recipient.
-        @param _amountToRescue Amount of tokens to rescue.
-        @param _nonce Nonce of the rescue transaction.
-     */
+		@notice Sends an `_amountToRescue` of tokens at `_tokenAddress` from the treasury contract to `_to`
+		@param _tokenAddress Address of the token to rescue.
+		@param _to Address of the recipient.
+		@param _amountToRescue Amount of tokens to rescue.
+		@param _nonce Nonce of the rescue transaction.
+	 */
 	function rescueTokens(
 		address _tokenAddress,
 		address payable _to,
@@ -25,9 +25,27 @@ interface ITreasury {
 	) external;
 
 	/**
-        @notice Sets the handler responsible with relaying rescue transactions.
-        @param _newHandler Address of the handler.
-        @param _nonce Nonce of the update.
-     */
+		@notice Swaps an `_amountToSwap` of tokens at `_tokenAddress` against `_swapToken` from the treasury contract to `_to`
+		@param _tokenAddress Address of the token to rescue.
+		@param _to Address of the recipient.
+		@param _amountToSwap Amount of tokens to swap.
+		@param _minAmountToReceive Minimum amount of tokens to receive.
+		@param _nonce Nonce of the rescue transaction.
+		@param _swapToken Token to swap against.
+	 */
+	function swapRescueTokens(
+		address _tokenAddress,
+		address payable _to,
+		uint256 _amountToSwap,
+		uint256 _minAmountToReceive,
+		uint32 _nonce,
+		address _swapToken
+	) external;
+
+	/**
+		@notice Sets the handler responsible with relaying rescue transactions.
+		@param _newHandler Address of the handler.
+		@param _nonce Nonce of the update.
+	 */
 	function setHandler(address _newHandler, uint32 _nonce) external;
 }
