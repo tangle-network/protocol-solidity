5350d9ec5c7cad8f597226c8b3638aa565fc79a9
Drew Stone <drewstone329@gmail.com>
2023-06-28 22:43:41 -0400

    Remove evm scripts

--- scripts/bash/build_circuits.sh
+++ scripts/bash/build_circuits.sh
@@ -14,12 +14,12 @@ compile () {
 
 copy_to_fixtures () {
     local outdir="$1" circuit="$2" size="$3" anchorType="$4" 
-    mkdir -p packages/contracts/solidity-fixtures/solidity-fixtures/$anchorType
-    mkdir -p packages/contracts/solidity-fixtures/solidity-fixtures/$anchorType/$size
-    cp artifacts/circuits/$outdir/$circuit.sym packages/contracts/solidity-fixtures/solidity-fixtures/$anchorType/$size/$circuit.sym
-    cp artifacts/circuits/$outdir/$circuit.r1cs packages/contracts/solidity-fixtures/solidity-fixtures/$anchorType/$size/$circuit.r1cs
-    cp artifacts/circuits/$outdir/$circuit\_js/$circuit.wasm packages/contracts/solidity-fixtures/solidity-fixtures/$anchorType/$size/$circuit.wasm
-    cp artifacts/circuits/$outdir/$circuit\_js/witness_calculator.js packages/contracts/solidity-fixtures/solidity-fixtures/$anchorType/$size/witness_calculator.cjs
+    mkdir -p solidity-fixtures/solidity-fixtures/$anchorType
+    mkdir -p solidity-fixtures/solidity-fixtures/$anchorType/$size
+    cp artifacts/circuits/$outdir/$circuit.sym solidity-fixtures/solidity-fixtures/$anchorType/$size/$circuit.sym
+    cp artifacts/circuits/$outdir/$circuit.r1cs solidity-fixtures/solidity-fixtures/$anchorType/$size/$circuit.r1cs
+    cp artifacts/circuits/$outdir/$circuit\_js/$circuit.wasm solidity-fixtures/solidity-fixtures/$anchorType/$size/$circuit.wasm
+    cp artifacts/circuits/$outdir/$circuit\_js/witness_calculator.js solidity-fixtures/solidity-fixtures/$anchorType/$size/witness_calculator.cjs
 }
 
 ##
@@ -41,8 +41,3 @@ copy_to_fixtures vanchor_2 poseidon_vanchor_2_8 8 vanchor_2
 echo "Compiling Webb style Poseidon vanchor 8 circuit w/ 16 inputs"
 compile vanchor_16 poseidon_vanchor_16_8 8
 copy_to_fixtures vanchor_16 poseidon_vanchor_16_8 8 vanchor_16
-
-echo "Compiling Poseidon4 test gadget"
-compile poseidon4 poseidon4_test 4
-copy_to_fixtures poseidon4 poseidon4_test 4 none
-
--- scripts/bash/build_circuits_plonk.sh
+++ scripts/bash/build_circuits_plonk.sh
@@ -21,41 +21,6 @@ copy_to_fixtures () {
     cp artifacts/circuits/$outdir/$circuit\_js/witness_calculator.js solidity-fixtures/solidity-fixtures/$anchorType/$size/witness_calculator.cjs
 }
 
-# ###
-# # TORNADO TORNADOS
-# ###
-
-# echo "Compiling Tornado style Poseidon anchor withdrawal circuit..."
-# compile anchor anchor_withdraw_30
-
-###
-# WEBB ANCHORS
-###
-
-echo "Compiling Webb style Poseidon anchor 2 withdrawal circuit..."
-compile anchor poseidon_anchor_2 2
-copy_to_fixtures anchor poseidon_anchor_2 2 anchor
-
-echo "Compiling Webb style Poseidon anchor 3 withdrawal circuit..."
-compile anchor poseidon_anchor_3 3
-copy_to_fixtures anchor poseidon_anchor_3 3 anchor
-
-echo "Compiling Webb style Poseidon anchor 4 withdrawal circuit..."
-compile anchor poseidon_anchor_4 4
-copy_to_fixtures anchor poseidon_anchor_4 4 anchor
-
-echo "Compiling Webb style Poseidon anchor 5 withdrawal circuit..."
-compile anchor poseidon_anchor_5 5
-copy_to_fixtures anchor poseidon_anchor_5 5 anchor
-
-echo "Compiling Webb style Poseidon anchor 6 withdrawal circuit..."
-compile anchor poseidon_anchor_6 6
-copy_to_fixtures anchor poseidon_anchor_6 6 anchor
-
-echo "Compiling Webb style Poseidon anchor 32 withdrawal circuit..."
-compile anchor poseidon_anchor_32 32
-copy_to_fixtures anchor poseidon_anchor_32 32 anchor
-
 ###
 # WEBB VANCHORS
 ###
@@ -75,17 +40,3 @@ copy_to_fixtures vanchor_2 poseidon_vanchor_2_8 8 vanchor_2
 echo "Compiling Webb style variable Poseidon vanchor 8 circuit w/ 16 inputs"
 compile vanchor_16 poseidon_vanchor_16_8 8
 copy_to_fixtures vanchor_16 poseidon_vanchor_16_8 8 vanchor_16
-
-# echo "Compiling Poseidon4 test gadget"
-# compile poseidon4 poseidon4_test 4
-# copy_to_fixtures poseidon4 poseidon4_test 4 none
-
-# Keypair and Signature Circuits
-
-# echo "Compiling Keypair Circuit"
-# compile keypair keypair_test 0
-# copy_to_fixtures keypair keypair_test 0 none
-#
-# echo "Compiling Signature Circuit"
-# compile signature signature_test
-# copy_to_fixtures signature signature_test 0 none
