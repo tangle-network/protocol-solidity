89dbde45410e7c8466f830cf5b591e3e65797d1a
Drew Stone <drewstone329@gmail.com>
2023-08-18 14:31:23 -0400

    V-WBT-VUL-027: Hardcoded Address Compatibility Problems

--- packages/contracts/contracts/utils/SanctionFilter.sol
+++ packages/contracts/contracts/utils/SanctionFilter.sol
@@ -7,6 +7,10 @@ import "../interfaces/external/chainalysis/ISanctionsList.sol";
 
 pragma solidity ^0.8.18;
 
+/// @title SanctionFilter contract.
+/// @notice This contract is used to filter out sanctioned addresses.
+/// @notice The hardcoded `SANCTIONS_CONTRACT` only exists on certain chains.
+/// Refer to https://go.chainalysis.com/chainalysis-oracle-docs.html for compatible chains.
 contract SanctionFilter {
 	address constant SANCTIONS_CONTRACT = 0x40C57923924B5c5c5455c48D93317139ADDaC8fb;
 
